90041acfe5fc2bd2ed6c7ae6b139ac5c95f347b7
lcy3155 <315508360@qq.com>
2017-11-04 20:16:02 +0800

    zsg1

--- houjs/routes/item.js
+++ houjs/routes/item.js
@@ -11,7 +11,7 @@ var con=mysql.createPool({
 /* GET users listing. */
 router.post('/list', function(req, res, next) {
   res.header('Access-Control-Allow-Origin',"*")
-  con.query('SELECT title,con FROM tj',function(err,rows,fields){
+  con.query('SELECT * FROM tj',function(err,rows,fields){
     if(err) throw err
     res.send(rows)
     console.log(rows)
@@ -20,11 +20,25 @@ router.post('/list', function(req, res, next) {
 
 router.post('/list1', function(req, res, next) {
   res.header('Access-Control-Allow-Origin',"*")
-  con.query('INSERT INTO tj (title,con) VALUES("'+req.body.abc+'","'+req.body.abb+'")',function(err,rows,fields){
+  con.query(`INSERT INTO tj (title,con) VALUES ('${req.body.abc}','${req.body.abb}')`,function(err,rows,fields){
     if(err) throw err
     res.send(rows)
     console.log(rows)
   })
 });
 
+router.post('/list3',function(req, res, next) {
+  res.header('Access-Control-Allow-Origin',"*")
+  con.query('DELETE FROM tj WHERE id='+req.body.acc,function(err, rows, fields) {
+   res.send(rows)
+  })
+});
+
+router.post('/list4',function(req, res, next) {
+  res.header('Access-Control-Allow-Origin',"*")
+  con.query('UPDATE tj SET title="'+req.body.xg1+'",con="'+req.body.xg2+'" WHERE id='+req.body.xg,function(err, rows, fields) {
+    res.send(rows)
+  })
+});
+     
 module.exports = router;
